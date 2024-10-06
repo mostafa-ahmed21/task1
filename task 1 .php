c0c323dd40be13a47e753f350ab72d5f32fdd9c7
mostafa-ahmed21 <mostafaahmad.ma34@gmail.com>
2024-10-06 22:51:48 +0300

    php file

---
+++ task 1 .php
@@ -0,0 +1,72 @@
+<?php
+
+//problem 1
+// $name = "programmer";
+// echo "Hello, ". $name;
+
+//problem 2
+// function calc ($num1,$num2) {
+//     $x=$num1;
+//     $y=$num2;
+//     echo "5 + 10 = ",$x + $y , "<br/>";
+//     echo "5 * 10 = ",$x * $y, "<br/>";
+//     echo "5 - 10 = ",$x - $y, "<br/>";
+
+// }
+// calc(10,5);
+
+//problem 3
+// function num ($A , $B ,$C ,$D ){
+//     echo  "Difference = " ,($A * $B) -($C * $D);
+// }
+// num( 1, 2, 3, 4);
+
+//problem 4
+// function last_deget($num1, $num2){
+//     $num1=$num1%10;
+//     $num2=$num2%10;
+//     echo $num1+$num2;
+// }
+// last_deget(6,13);
+
+//problem 5
+// function calc ($num1) {
+//     $x=$num1/1000;
+//     if($x%2==0){
+//         echo "EVEN <br/>";
+//     }else{
+//         echo "ODD <br/>";
+//     }
+// }
+//  calc(4569);
+//  calc(3569);
+
+//problem 6
+// function game ($a ,$b ,$k ){
+//     $num1= $a % $k ;
+//     $num2= $b % $k ;
+//     if($num1 == 0 && $num2 == 0){
+//         echo "Both <br/>";
+//     }else if($num1 == 0 ){
+//         echo "memo <br/>";
+//     }else{
+//         echo "momo <br/>";
+//     }
+// }
+//  game (15,7,3);
+//  game (22,10,2);
+
+//problem 7
+// function lucky (int$N){
+//     $first=$N % 10;
+//     $last=(int)($N / 10);
+//     if($first % $last ==0 || $last % $first ==0){
+//         echo "YES <br/>";
+//     }else{
+//         echo "NO <br/>";
+//     }
+// }
+// lucky(36);
+// lucky(46);
+
+?>
